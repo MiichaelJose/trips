d3e04b8dd9108267d849e6f78e18a0858380d1d9
MiichaelJose <michaeljosecraft1@gmail.com>
2024-07-11 23:41:18 -0300

    create-trip.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,26 +1,10 @@
 import fastify from "fastify"; // framework
 
-import { prisma } from "./lib/prisma";
+import { createTrip } from "./routes/create-trip";
 
 const app = fastify()
 
-app.get("/cadastrar", async () => {
-    await prisma.trip.create({
-        data: {
-            destination: "SP",
-            starts_at: new Date(),
-            ends_at: new Date()
-        },
-    })
-
-    return "Registro cadastrado com sucesso"
-})
-
-app.get("/listar", async () => {
-    const trips = await prisma.trip.findMany()
-
-    return trips
-})
+app.register(createTrip)
 
 app.listen({ port: 3333 }).then(() => {
     console.log("oi");
